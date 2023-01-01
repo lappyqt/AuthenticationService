23cbbceea81325242684d2bb7542fae7ac80b0e7
Vladislav <vldp2017@gmail.com>
2023-01-02 00:08:27 +0500

    Configuration

--- src/AuthenticationService.API/Program.cs
+++ src/AuthenticationService.API/Program.cs
@@ -14,11 +14,10 @@ builder.Services.AddJWT(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/", () => "API is working...");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
